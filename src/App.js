027af57dac80a16be37a712e318a06b2d19470fb
Closure <dmitrijs.asanovs@gmail.com>
2020-08-01 14:04:48 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  const renderContent = () => (
+    <>
+      <div className="Parallax__content__heading">
+        <h1 className="Parallax__content__heading__text">Closure</h1>
+        <h2 className="Parallax__content__heading__caption">
+          Your one-stop source of Web Development tricks
+        </h2>
+      </div>
+      <div className="Parallax__content__cta">
+        <p>
+          <b>1. Like the video.</b> Because it helps me and my channel
+        </p>
+        <p>
+          <b>2. Like the video.</b> To see more content like that!
+        </p>
+        <p>
+          <b>3. Follow the Github link.</b> And play with this code yourself!
+        </p>
+      </div>
+    </>
+  );
+
+  return (
+    <section className="Parallax">
+      <div className="Parallax__background" />
+      <div className="Parallax__background-triangles" />
+      <div className="Parallax__content">{renderContent()}</div>
+    </section>
+  );
+}
+
+export default App;
